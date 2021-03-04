e7a802c42c8c0d4f75db0bc9433198d3b7086a7e
Joe Boris <josephdboris@gmail.com>
2021-03-04 00:03:54 -0500

    Updated newInput
    -updated newInput to my most recent version

--- todo-list.js
+++ todo-list.js
@@ -143,15 +143,37 @@ function E(html) {
   return template.content.firstChild;
 }
 
-function newInput(html, boundObject) {
-  let input = E(html);
+// Return a new element bound (both ways) to the appropriate key of the given object
+// options: an array of objects with 'id' and 'name' properties
+function newInput(html, object, options = []) {
+  let element = E(html);
 
-  input.value = boundObject[input.name];
-  input.oninput = () => {
-    boundObject[input.name] = input.value;
+  if (element.tagName.toLowerCase() == "select") {
+    for (let option of options) {
+      // option: { id: 1, name: "USA" }
+      let optionElement = E(`<option></option>`);
+      optionElement.value = option.id;
+      optionElement.innerText = option.name;
+
+      element.append(optionElement);
+    }
+
+    object[element.name] = options[0].id;
+  }
+
+  if (object[element.name] != null) {
+    if (element.type == "checkbox") {
+      element.checked = object[element.name];
+    } else {
+      element.value = object[element.name];
+    }
+  }
+
+  element.oninput = () => {
+    object[element.name] =
+      element.type == "checkbox" ? element.checked : element.value;
   };
-
-  return input;
+  return element;
 }
 
 function insertHere(element) {
